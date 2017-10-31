81d4b50d3f4e0a7ab16e96439a5aaf469349992d
sceneturkey <33245995+sceneturkey@users.noreply.github.com>
2017-10-31 02:55:30 -0500

    Update prime_tester_list.py

--- prime_tester_list.py
+++ prime_tester_list.py
@@ -14,12 +14,13 @@ for x in range(y, z+1):
         continue
     for i in range(x, 1, -1):
         if (i == 2 and is_prime == True):
-            print(x)
+            primes.append(x)
             continue
         elif (x % (i-1) == 0):
             is_prime = False
             continue
         else:
             continue
+print(primes)
 
         
