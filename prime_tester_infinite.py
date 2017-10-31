17d4bc3874f1b8471b81f05fc257b2bbe6ee4b15
sceneturkey <33245995+sceneturkey@users.noreply.github.com>
2017-10-31 05:27:05 -0500

    Update prime_tester_infinite.py

--- prime_tester_infinite.py
+++ prime_tester_infinite.py
@@ -1,16 +1,19 @@
 #Tests all numbers and prints all primes
 
+primes = []
 x = 0
 while(True):
     x += 1
     is_prime = True
     if (x == 1 or x == 2):
+        primes.append[x]
         print(x)
         continue
     elif (x % 2 == 0):
         continue
     for i in range(x, 1, -1):
         if (i == 2 and is_prime == True):
+            primes.append[x]
             print(x)
             continue
         elif (x % (i-1) == 0):
